0e7d579936eccb6bee40e5a77fb186dafbd1bc03
נור <njuidatnor750@gmail.com>
2024-09-10 19:55:10 +0300

    game.js
    הגדררת כול המשתנים של משחק

--- game.js
+++ game.js
@@ -3,3 +3,10 @@ let moveSpeed = 2;
 let gravity = 0.5;
 let pipeGap = 45;
 let pipeSeparation = 150;
+let bird = document.querySelector('.bird');
+let background = document.querySelector('.background').getBoundingClientRect();
+let scoreVal = document.querySelector('.score_val');
+let message = document.querySelector('.message');
+let scoreTitle = document.querySelector('.score_title');
+
+let gameState = 'Start';
