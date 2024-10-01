50457cf288338cce4cc647688ffe75a1f9def299
Mohammed Zoabi <zhamodi@gmail.com>
2024-10-01 18:38:12 +0300

    Update game.js
    שינוי דרך התזמון במשחק במקום לעבוד עם 
    animation frame 
    נעבוד עם InterVal

--- game.js
+++ game.js
@@ -1,39 +1,13 @@
 // Basic game settings
-let moveSpeed = 2;
-let gravity = 0.5;
-let pipeGap = 45;
-let pipeSeparation = 150;
+let moveSpeed = 4; // מהירות ציפור
+let gravity = 1; // מהיות נפילה 
+let pipeGap = 70;  // המרחק בין העמודים בהתחלה 
+let pipeSeparation = 150; // התחלה עם פחות עמודים
+let intervalTime = 16; // מהירות קבועה
+
+
 let bird = document.querySelector('.bird');
 let background = document.querySelector('.background').getBoundingClientRect();
 let scoreVal = document.querySelector('.score_val');
 let message = document.querySelector('.message');
 let scoreTitle = document.querySelector('.score_title');
-
-let gameState = 'Start';
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Enter') {
-    if (gameState === 'Start' || gameState === 'End') {
-      startGame();
-    }
-  }
-});
-function resetGame() {
-  document.querySelectorAll('.pipe_sprite').forEach(function (pipe) {
-    pipe.remove();
-  });
-
-  bird.style.top = '40vh';
-  birdDy = 0;
-  gameState = 'Start';
-}
-function startGame() {
-  resetGame();
-  gameState = 'Play';
-  message.innerHTML = '';
-  scoreTitle.innerHTML = 'Score: ';
-  scoreVal.innerHTML = '0';
-  moveSpeed = 2;
-  pipeGap = 45;
-  pipeSeparation = 150;
-  playGame();
-}
