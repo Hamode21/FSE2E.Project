f1a5ec80d3ca0316a1e435837935d2efce6240d9
Mohammed Zoabi <zhamodi@gmail.com>
2024-10-08 12:22:39 +0300

    Update game.js
    הוספת את
    פונקציה לתנועת הצינורות ובודקת אם יש פגיעה במכשול

--- game.js
+++ game.js
@@ -56,3 +56,37 @@ function playGame() {
   createPipe();
 }
 
+// פונקציה לתנועת הצינורות ובודקת אם יש פגיעה במכשול
+function movePipes() {
+  if (gameState !== 'Play') return;
+
+  let pipes = document.querySelectorAll('.pipe_sprite');
+  pipes.forEach(function (pipe) {
+    let pipePos = pipe.getBoundingClientRect();
+    let birdPos = bird.getBoundingClientRect();
+
+    if (pipePos.right <= 0) {
+      pipe.remove();
+    } else if (
+      birdPos.left < pipePos.left + pipePos.width - 10 &&
+      birdPos.left + birdPos.width > pipePos.left &&
+      birdPos.top < pipePos.top + pipePos.height - 10 &&
+      birdPos.top + birdPos.height > pipePos.top
+    ) {
+      gameState = 'End';
+      message.innerHTML = 'Press Enter To Restart';
+      clearInterval(gameInterval); // עוצר את המשחק כשמפסידים
+    } else {
+      if (
+        pipePos.right < birdPos.left &&
+        pipePos.right + moveSpeed >= birdPos.left &&
+        pipe.getAttribute('increase_score') === '1'
+      ) {
+        scoreVal.innerHTML = parseInt(scoreVal.innerHTML) + 1;
+        adjustDifficulty(parseInt(scoreVal.innerHTML));
+      }
+      pipe.style.left = pipePos.left - moveSpeed + 'px';
+    }
+  });
+}
+
